877c9a6b774873ef5515d62ea3699ec5ddf8edd1
IVC1-5 <ivanbessmertnykhphysics@gmail.com>
2021-09-11 10:44:49 +0300

    praktika1

---
+++ src/com/example/helloworld/MasSum.java
@@ -0,0 +1,40 @@
+package com.example.helloworld;
+
+import java.util.Scanner;
+
+public class MasSum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+//        int[] a = new int[5];
+//        int n;
+//        int Sum = 0;
+//        System.out.println("Enter numberofarrays");
+//        n = in.nextInt();
+//        a = new int [n];
+//        for (int i =0; i<n; i ++) {
+//            a [i] = (int) Math.round((Math.random() * 20));
+//            System.out.println("a[" + i + "] = " + a[i] );
+//          //  Sum += a[i];
+//        }
+//        int j = 0;
+//        while (j<n) {
+//            Sum += a[j];
+//            j += 1;
+//        }
+//        do {
+//           Sum += a[j];
+//           j += 1;
+//        } while (j<n);
+//        System.out.println(Sum);
+
+//        int a = 10;
+//        for (int i = 0; i<a; i++) {
+//            System.out.println(i);
+//        }
+
+        int n;
+        for (int i = 1;)
+
+    }
+
+}
